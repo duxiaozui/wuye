4a254473bc09d1d4f099c71b9787674d3ec92aca
嘟小嘴 <729536321@qq.com>
2023-07-24 11:11:06 +0800

    修改配置端口号

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    port: 9999
+  }
 })
+// 配置端口信息，关闭校验
