c6bfc224ee8b36199a096407576dd79ea11ce0a3
嘟小嘴 <729536321@qq.com>
2023-07-24 14:00:59 +0800

    创建前端工程完成

---
+++ src/components/login/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>登录</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="less" scoped></style>
